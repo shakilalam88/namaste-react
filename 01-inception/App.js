03222998b733d649e257ceba08a6ade123717b4c
shakil_alam <shakilalam88@gmail.com>
2024-02-28 17:08:38 +0530

    episode-01

---
+++ 01-inception/App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', { class: 'h1' }, 'This is h1'),
+    React.createElement('h2', { class: 'h2' }, 'This is h2'),
+  ])
+)
+
+// const heading = React.createElement(
+//   'h1',
+//   { id: 'heading', class: 'heading' },
+//   'hello world form React!'
+// )
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(heading)
